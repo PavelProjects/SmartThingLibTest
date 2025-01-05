6519513e882e9eb169dda6638748190d84f00631
PavelProjects <pavelprojects52@gmail.com>
2025-01-06 00:21:16 +0300

    api update

--- src/main.cpp
+++ src/main.cpp
@@ -24,56 +24,50 @@ void setup() {
     });
     ArduinoOTA.onError([](ota_error_t err) {
       st_log_warning("main", "Ota update error! code=%d", err);
-    });
+    }); 
     ArduinoOTA.onEnd([]() {
       st_log_warning("main", "Ota update finsihed!");
     });
     
-    #ifdef ARDUINO_ARCH_ESP32
-    ArduinoOTA.setMdnsEnabled(false);
-    #endif
-    #ifdef ARDUINO_ARCH_ESP8266
-    ActionsManager.call("led_on");
-    #endif
-    ArduinoOTA.begin();
   }
+  
+  #ifdef ARDUINO_ARCH_ESP8266
+  ActionsManager.call("led_on");
+  #endif
 
   st_log_info("main", "Setup finished");
 }
 
 void loop() {
-  if (SmartThing.wifiConnected()) {
-    ArduinoOTA.handle();
-  }
   //Your loop logic here
   #ifdef ARDUINO_ARCH_ESP8266
-  SmartThing.loop();
+    SmartThing.loop();
   #endif
   delay(250);
 }
 
 void addActions() {
   #if ENABLE_ACTIONS 
-  ActionsManager.add("led_off", "Turn led off", []() {
-    digitalWrite(LED_PIN, LOW);
-    return ActionResult(true);
-  });
-  ActionsManager.add("led_on", "Turn led on", []() {
-    digitalWrite(LED_PIN, HIGH);
-    return ActionResult(true);
-  });
+    ActionsManager.add("led_off", "Turn led off", []() {
+      digitalWrite(LED_PIN, LOW);
+      return ActionResult(true);
+    });
+    ActionsManager.add("led_on", "Turn led on", []() {
+      digitalWrite(LED_PIN, HIGH);
+      return ActionResult(true);
+    });
   #endif
 }
 
 void addSensors() {
   #if ENABLE_NUMBER_SENSORS
-  SensorsManager.addDigitalSensor("button", SENSOR_BUTTON_PIN);
+    SensorsManager.addDigitalSensor("button", SENSOR_BUTTON_PIN);
   #endif
   #if ENABLE_TEXT_SENSORS 
-  SensorsManager.addSensor("led", []() {
-    // Пример возможной логики вычисления значения
-    return digitalRead(LED_PIN) == HIGH ? "on" : "off"; 
-  });
+    SensorsManager.addSensor("led", []() {
+      // Пример возможной логики вычисления значения
+      return digitalRead(LED_PIN) == HIGH ? "on" : "off"; 
+    });
   #endif
 }
 
